d8eb11b7a74b21fa8ac74297bc486b29a2977c00
Irfan Sethi <corgi.orange7@gmail.com>
2025-07-30 18:33:46 -0400

    Changed folder structure

---
+++ Monitor.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MonitorProfiler.Win32;
+
+namespace MonitorProfiler.Models.Display
+{
+    public struct MonitorFeature
+    {
+        public bool IsSupported;
+        public uint Min, Max, Current, Original;
+    }
+
+    public enum FeatureType
+    {
+        Brightness,
+        Contrast,
+        RedDrive, GreenDrive, BlueDrive,
+        RedGain, GreenGain, BlueGain
+    }
+
+    public class Monitor: IDisposable
+    {
+        #region Declarations
+        public IntPtr HPhysicalMonitor { get; set; }
+        public int Index { get; set; }
+        public string Name { get; set; }
+        public Object Tag { get; set; }
+
+        public MonitorFeature Brightness;
+        #endregion
+        
+        public Monitor(NativeStructures.PHYSICAL_MONITOR physicalMonitor)
+        {
+            HPhysicalMonitor = physicalMonitor.hPhysicalMonitor;
+            Name = physicalMonitor.szPhysicalMonitorDescription;
+
+            Brightness.IsSupported = NativeMethods.GetMonitorBrightness(HPhysicalMonitor, ref Brightness.Min, ref Brightness.Current, ref Brightness.Max);
+            Brightness.Original = Brightness.Current;
+        }
+
+        /// <summary>
+        /// Disposes the monitor
+        /// </summary>
+        public void Dispose()
+        {
+            NativeMethods.DestroyPhysicalMonitor(HPhysicalMonitor);
+        }
+
+        /// <summary>
+        /// Sets brightness to a value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool SetBrightness(uint value)
+        {            
+            bool retVal =  NativeMethods.SetMonitorBrightness(HPhysicalMonitor, Brightness.Current);
+            if (retVal)
+                Brightness.Current = value;
+
+            return retVal;
+        }
+        
+        public void SetCurrentBrightnessAsOriginal()
+        {
+            Brightness.Original = Brightness.Current;
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+    }
+}
---
+++ ToolsControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace MonitorProfiler
+{
+    public partial class ToolsControl : UserControl
+    {
+        private int SC_MONITORPOWER = 0xF170;
+        private uint WM_SYSCOMMAND = 0x0112;
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+
+        public ToolsControl()
+        {
+            InitializeComponent();
+        }
+
+        private void StandByButton_Click(object sender, EventArgs e)
+        {
+            SendMessage(this.Handle, WM_SYSCOMMAND, (IntPtr)SC_MONITORPOWER, (IntPtr)2);
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
